502e2cb7ab95cd1622f60638c9e36c1a7706c059
Lev Khoriak <lev.horyak@gmail.com>
2025-04-07 12:56:50 +0800

    MPT tool rework based on the review comments

--- gui_mpt_optimizer.py
+++ gui_mpt_optimizer.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import pandas as pd
+import textwrap
 from mpt_module import mpt_optimizer
 # from mpt_optimizer import mpt_optimizer
 # from mpt_module import mpt_optimizer
 
-st.write(
-    '''
+input_format = '''
     # Input format
     This tool expects your CSV files to have the following headers:
     | TICKER | DATE | CLOSE |
@@ -14,42 +14,45 @@ st.write(
 
     The data is expected to be comma-delimeted.
 '''
-)
 
-st.write(
-    '''
+upload_files_string = '''
     # Upload your files
 '''
-)
 
+def main():
 
-uploaded_files = st.file_uploader("Upload your CSV files with asset prices", accept_multiple_files=True)
-assets_dataframes = []
+    st.write(textwrap.dedent(input_format))
 
-# Initialize with values that will surely be overwritten
-begindate, enddate = (pd.Timestamp('1911-01-01'), pd.Timestamp('2026-01-01'))
+    st.write(textwrap.dedent(upload_files_string))
 
-if uploaded_files is not None:
+    uploaded_files = st.file_uploader("Upload your CSV files with asset prices", accept_multiple_files=True)
+    assets_dataframes = []
+
+    # Initialize with values that will surely be overwritten
+    begindate, enddate = (pd.Timestamp('1911-01-01'), pd.Timestamp('2026-01-01'))
+
+    if len(uploaded_files) < 2:
+        return
+    
     for file in uploaded_files:
         try:
             df = pd.read_csv(filepath_or_buffer=file, delimiter=',', parse_dates=[1])
             df.columns = ['Ticker', 'Date', 'Close']
-
-            # Find a common timerange that will fit inside all the time series
-            begindate = max(begindate, df['Date'].min())
-            enddate = min(enddate, df['Date'].max())
-            assets_dataframes.append(df)
         except:
             st.error(
                 f'Could not parse the following CSV file: {file.name}.\n\n'
                 f'Make sure that the headers and delimiters are in the correct format.'
                 )
+        # Find a common timerange that will fit inside all the time series
+        begindate = max(begindate, df['Date'].min())
+        enddate = min(enddate, df['Date'].max())
+        assets_dataframes.append(df)
 
-# If there are no common dates for all the assets
-if begindate > enddate - pd.Timedelta(days=10):
-    st.error('No common timeframe could be chosen.')
+    # If there are no common dates for all the assets
+    if begindate > enddate - pd.Timedelta(days=10):
+        st.error('No common timeframe could be chosen.')
+        return
 
-if len(uploaded_files) > 1 and begindate <= enddate - pd.Timedelta(days=10):
     st.write('# Construct the portfolio')
     d = st.date_input(
         label='Portfolio construction date',
@@ -59,9 +62,12 @@ if len(uploaded_files) > 1 and begindate <= enddate - pd.Timedelta(days=10):
         )
     required_return = st.number_input(label='What is the required return (as a decimal)?', placeholder='optional', value=None, step=0.01)
 
-    if len(assets_dataframes) > 1:
-        try:
-            fig = mpt_optimizer(assets_dataframes, str(d.year), str(d.month), str(d.day), 0 if required_return == None else required_return)
-            st.pyplot(fig)
-        except:
-            st.error('Failed to run the algorithm')
+    date = f'{d.year}-{d.month}-{d.day}'
+    try:
+        fig = mpt_optimizer(assets_dataframes, date, 0 if required_return == None else required_return, 10)
+        st.pyplot(fig)
+    except:
+        st.error('Failed to run the algorithm')
+
+if __name__ == '__main__':
+    main()
--- mpt_module.py
+++ mpt_module.py
@@ -4,78 +4,80 @@ import scipy.optimize
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def mpt_optimizer(assets_files, year, month, day, required_return):
+# Weighted average return using matrix multiplication
+def gen_return_formula(R):
+    def gen_return(W):
+        return np.matmul(W, R)
+    return gen_return
 
-    
-    assets_n = len(assets_files)
-    tickers = []
+# Total portfolio standard deviation = sqrt(W * Cov * W^T)
+def gen_std_formula(Cov):
+    def gen_std(W):
+        return np.sqrt(np.matmul(np.matmul(W, Cov), W.transpose()))
+    return gen_std
 
-    assets = pd.DataFrame()
+# Sum of weights should be equal to one
+def sum_of_weights(W):
+    return np.sum(W) - 1
+
+# Annualized return should be no less than required return
+def return_constraint_formula(gen_return, period, required_return):
+    def return_constraint(W):
+        return (1 + gen_return(W))**period - 1 - required_return
+    return return_constraint
+
+
+def mpt_optimizer(assets_files, date, required_return, window_size):
+
+    asset_n = len(assets_files)
 
     # Read close price data, add to database
-    for asset_df in assets_files:
-        assets = pd.concat([assets, asset_df])
+    assets = pd.concat(assets_files)
 
     # Compute daily returns
     assets['Returns'] = assets.groupby('Ticker')['Close'].transform(lambda x: x / x.shift(1) - 1)
     assets.dropna(axis=0, inplace=True)
 
-    window_size = 10
-
     # Pivot for later use of covariance and expected return
     returns_pivot = assets.pivot(index='Date', columns='Ticker', values='Returns')
+    returns_pivot = returns_pivot.sort_index()
 
     # Compute rolling covariance matrix and expected returns
-    returns_pivot = returns_pivot.sort_index()
     covm = returns_pivot.rolling(window=window_size).cov()
-    expected_returns = returns_pivot.rolling(window=window_size).mean()
     covm.dropna(axis=0, inplace=True)
+
+    expected_returns = returns_pivot.rolling(window=window_size).mean()
     expected_returns.dropna(axis=0, inplace=True)
 
-    Date = year + '-' + month + '-' + day
-
-    # Find data for the required date
-    Rmatrix = expected_returns.loc[Date].values
-    Covmatrix = covm.loc[Date].values
-
-    # Weighted average return using matrix multiplication
-    def gen_return(W, R=Rmatrix):
-        return np.matmul(W, R)
-
-    # Total portfolio standard deviation = sqrt(W * Cov * W^T)
-    def gen_std(W, Cov=Covmatrix):
-        return np.sqrt(np.matmul(np.matmul(W, Cov), W.transpose()))
+    # Rmatrix is the matrix of returns and Covmatrix is the covariance matrices
+    Rmatrix = expected_returns.loc[date].values
+    Covmatrix = covm.loc[date].values    
 
     # Initial weights (0.2 chosen randomly)
-    W0 = np.array([0.2] * assets_n)
-    bounds = [(0, 1) for _ in range(assets_n)]
-
-    # Sum of weights should be equal to one
-    def sum_of_weights(W):
-        h = np.sum(W) - 1
-
-        return h
-
-    constraints = ({'type': 'eq', 'fun': sum_of_weights})
-
-    # Minimum risk portfolio without return constraint
-    global_minimum = scipy.optimize.minimize(fun=gen_std, x0=W0, bounds=bounds, method='SLSQP', constraints=constraints)
-
+    W0 = np.array([0.2] * asset_n)
+    bounds = [(0, 1) for _ in range(asset_n)]
 
     # Random sampling from Dirichlet distribution to get array of random weights 
-    W = np.random.dirichlet(np.ones(assets_n), 2000)
+    W = np.random.dirichlet(np.ones(asset_n), 2000)
+
+    gen_return = gen_return_formula(Rmatrix)
+    gen_std = gen_std_formula(Covmatrix)
 
     period = 360 / window_size
+    return_constraint = return_constraint_formula(gen_return, period, required_return)
 
-    # Annualized return should be no less than required return
-    def return_constraint(W):
-        g = (1 + gen_return(W))**period - 1 - required_return
-        return g
+    def find_minvar_portfolio(constraints):
+        return scipy.optimize.minimize(fun=gen_std, x0=W0, bounds=bounds, method='SLSQP', constraints=constraints)
 
-    constraints = ({'type': 'eq', 'fun': sum_of_weights}, {'type': 'ineq', 'fun': return_constraint})
+    # Minimum risk portfolio without return constraint
+    global_minimum = find_minvar_portfolio(
+        ({'type': 'eq', 'fun': sum_of_weights})
+    )
 
     # Minimum risk portfolio with return contraint
-    constrained_minimum = scipy.optimize.minimize(fun=gen_std, x0=W0, bounds=bounds, method='SLSQP', constraints=constraints)
+    constrained_minimum = find_minvar_portfolio(
+        ({'type': 'eq', 'fun': sum_of_weights}, {'type': 'ineq', 'fun': return_constraint})
+    )
 
     # Generate annualized total portfolio risk for random weights
     V = np.multiply(list(map(gen_std, W)), np.sqrt(period))
@@ -86,25 +88,30 @@ def mpt_optimizer(assets_files, year, month, day, required_return):
     # Annualize returns
     R = np.power(np.add(R, 1), period) - 1
 
+    def annualize_risk_ret(W):
+        min_std_pa = gen_std(W) * np.sqrt(period)
+        min_ret_pa = (1 + gen_return(W))**period - 1
+        return (min_std_pa, min_ret_pa)
+
     # Annualize risks and returns of unconstrained portfolio
-    global_minimum_std = gen_std(global_minimum.x) * np.sqrt(period)
-    global_minimum_ret = (1 + gen_return(global_minimum.x))**period - 1
+    (global_minimum_std, global_minimum_ret) = annualize_risk_ret(global_minimum.x)
 
     # Annualize risks and returns of constrained portfolio
-    constrained_minimum_std = gen_std(constrained_minimum.x) * np.sqrt(period)
-    constrained_minimum_ret = (1 + gen_return(constrained_minimum.x))**period - 1
+    (constrained_minimum_std, constrained_minimum_ret) = annualize_risk_ret(constrained_minimum.x)
 
     fig, ax = plt.subplots()
+
     # Plot efficient frontier
     sns.scatterplot(x = V, y = R, ax=ax)
     if global_minimum.success:
         ax.plot(global_minimum_std, global_minimum_ret, 'ro')
     if constrained_minimum.success:
         ax.plot(constrained_minimum_std, constrained_minimum_ret, 'go')
-    elif not constrained_minimum.success:
+    else:
         print('Failed to find optimum portfolio with this required return')
-    plt.legend(['possible portfolio', 'least risky', f'least risky with return $\\geq$ {required_return}'])
-    plt.title(f'Efficient frontier at {Date}')
+
+    plt.legend(['possible portfolio', 'least risky', f'least risky with return $\\geq$ {required_return:.2f}'])
+    plt.title(f'Efficient frontier at {date}')
     plt.xlabel('Risk (annualized)')
     plt.ylabel('Return (annualized)')
     return fig
